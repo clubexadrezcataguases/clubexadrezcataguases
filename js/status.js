65797b64bcb1ad2e9eede6b80cdfe28c896c3452
clubexadrezcataguases <mariacerqueirabot@gmail.com>
2024-09-04 13:56:28 -0300

    Create status.js

---
+++ js/status.js
@@ -0,0 +1,11 @@
+function updateOnlineStatus() {
+  const offlineNotice = document.getElementById('offline-notice');
+  if (!navigator.onLine) {
+    offlineNotice.style.display = 'block';
+  } else {
+    offlineNotice.style.display = 'none';
+  }
+}
+window.addEventListener('online', updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);
+updateOnlineStatus();
