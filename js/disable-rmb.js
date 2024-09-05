6debf66063858d4f48db66976e92cfee968ddb40
clubexadrezcataguases <mariacerqueirabot@gmail.com>
2024-09-05 16:52:28 -0300

    Create disable-rmb.js
    function

---
+++ js/disable-rmb.js
@@ -0,0 +1,3 @@
+        document.addEventListener('contextmenu', function(event) {
+            event.preventDefault(); 
+        }); // desativa o botão direito do mouse em determinadas páginas selecionadas
